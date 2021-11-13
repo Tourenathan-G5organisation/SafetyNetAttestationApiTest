778cbb9c1c9d113c4051eaa81f0543a419211ae2
Teyou Toure <toure21@live.com>
2021-11-13 10:58:03 +0100

    Fixed errors and permission  issues

--- app/src/main/java/com/example/myapplicationtest/api/SafetyNetService.java
+++ app/src/main/java/com/example/myapplicationtest/api/SafetyNetService.java
@@ -1,11 +1,12 @@
 package com.example.myapplicationtest.api;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface SafetyNetService {
 
     @POST("/androidcheck/v1/attestations/verify")
-    Call<VerificationResponse> verifyReQuest(@Query("key") String key, VerificationRequest request);
+    Call<VerificationResponse> verifyReQuest(@Query("key") String key, @Body VerificationRequest request);
 }
